63c4b300fc7008f50bb0c931c0769c3ed2fcf2c2
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-11 16:25:06 -0600

    adding components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "some description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272"),
+    new Recipe("Test Recipe 2", "some description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
+  ];
+}
