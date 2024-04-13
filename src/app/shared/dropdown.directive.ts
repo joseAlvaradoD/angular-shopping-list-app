30c2d34f28cceab08fb60fa65145fc45b33f04c0
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-13 13:11:10 -0600

    add toggle directive for dropdown menus

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+
+  @HostBinding('class.open') isOpen: boolean = false;
+
+  constructor(private elRef: ElementRef) {}
+
+  @HostListener('document:click') toggleOpen(eventData: Event){
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+
+}
