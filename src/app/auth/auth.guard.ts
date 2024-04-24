46da722e5c2c7fc67a72fd259b26f7747be3b796
JoseAlvarado <jose.dejesusalvarado@evertecinc.com>
2024-04-24 11:52:18 -0600

    only check once at authGuard if user is logged in

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthService } from "./auth.service";
-import { map } from "rxjs/operators";
+import { map, take } from "rxjs/operators";
 
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate{
@@ -10,6 +10,7 @@ export class AuthGuard implements CanActivate{
 
  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
    return this.authService.user.pipe(
+    take(1),
     map(user => {
       return !!user? true : this.router.createUrlTree(['/auth']);
     })
